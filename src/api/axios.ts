751f6597af8c37c7489bae3deaf8b9a8778f7726
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-05-12 23:16:36 +0300

    fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://cinema-library.onrender.com/api/',
+    baseURL: 'https://cinema-library.onrender.com/api',
     withCredentials: true,
 });
 
